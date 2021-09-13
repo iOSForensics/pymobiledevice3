70e7b747a6e64fe694c69c32715ecb5d9c6c4eea
Mathieu Renard <mathieu.renard@gotohack.org>
2021-09-13 23:07:24 +0200

    Add pull command

--- pymobiledevice3/services/house_arrest.py
+++ pymobiledevice3/services/house_arrest.py
@@ -13,16 +13,17 @@ class HouseArrestService(AfcService):
         service_name = self.SERVICE_NAME
         super(HouseArrestService, self).__init__(self.lockdown, service_name)
 
-    def send_command(self, bundle_id, cmd='VendContainer'):
+    def send_command(self, bundle_id, cmd='VendDocuments'):
         self.service.send_plist({'Command': cmd, 'Identifier': bundle_id})
         res = self.service.recv_plist()
+        print(res)
         if res.get('Error'):
             self.logger.error('%s: %s', bundle_id, res.get('Error'))
             return False
         else:
             return True
 
-    def shell(self, application_id, cmd='VendContainer'):
+    def shell(self, application_id, cmd='VendDocuments'):
         res = self.send_command(application_id, cmd)
         if res:
             AfcShell(self.lockdown).cmdloop()
