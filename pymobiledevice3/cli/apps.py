ffa765f01d1ccf8d020f9521dc992088b0a50019
Mathieu Renard <mathieu.renard@gotohack.org>
2021-09-13 23:08:01 +0200

    Add pull command

--- pymobiledevice3/cli/apps.py
+++ pymobiledevice3/cli/apps.py
@@ -1,9 +1,11 @@
 import click
+import logging
 
 from pymobiledevice3.cli.cli_common import Command, print_json
 from pymobiledevice3.services.house_arrest import HouseArrestService
 from pymobiledevice3.services.installation_proxy import InstallationProxyService
-
+from pymobiledevice3.services.afc import AfcShell, AfcService
+import os
 
 @click.group()
 def cli():
@@ -19,16 +21,28 @@ def apps():
 
 @apps.command('list', cls=Command)
 @click.option('--color/--no-color', default=True)
+@click.option('--json/--no-json', default=True)
 @click.option('-u', '--user', is_flag=True, help='include user apps')
 @click.option('-s', '--system', is_flag=True, help='include system apps')
-def apps_list(lockdown, color, user, system):
+def apps_list(lockdown, color, json, user, system):
     """ list installed apps """
     app_types = []
     if user:
         app_types.append('User')
     if system:
         app_types.append('System')
-    print_json(InstallationProxyService(lockdown=lockdown).get_apps(app_types), colored=color)
+    apps_list = InstallationProxyService(lockdown=lockdown).get_apps(app_types)
+    if json:
+        print_json(apps_list, colored=color)
+    else:
+        for app in apps_list:
+            if app.get("ApplicationType") != "System":
+                if app["CFBundleIdentifier"] == "fr.nartex.visorando":
+                    print(app)
+                    exit()
+                print(app["CFBundleIdentifier"], "=>", app.get("Container"))
+            else:
+                print(app["CFBundleIdentifier"], "=> N/A")
 
 
 @apps.command('uninstall', cls=Command)
@@ -50,3 +64,23 @@ def install(lockdown, ipa_path):
 def afc(lockdown, bundle_id):
     """ open an AFC shell for given bundle_id, assuming its profile is installed """
     HouseArrestService(lockdown=lockdown).shell(bundle_id)
+
+@apps.command('pull', cls=Command)
+@click.argument('src', type=click.Path(file_okay=True, dir_okay=True, exists=False), nargs=1)
+@click.argument('dst', type=click.Path(file_okay=False, dir_okay=True, exists=False), nargs=1)
+@click.argument('bundle_id', nargs=1)
+def pull(lockdown, src, dst, bundle_id):
+    """ pull application files with AFC service for given bundle_id, assuming its profile is installed """
+    print(bundle_id,src, dst)
+    if not os.path.exists(dst):
+        os.makedirs(dst)
+
+    def log(src, dst):
+        logging.info(f'{src} --> {dst}')
+
+    client = HouseArrestService(lockdown=lockdown)
+    client.send_command(bundle_id, cmd='VendDocuments')
+    print(client)
+    #client.listdir(src)
+    client.pull(src, dst, callback=log)
+
